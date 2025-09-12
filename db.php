cf7117c4d2f797d9024e5c9735fc6712adc5e44b
Khanh <lamthidiemkhanh@gmail.com>
2025-09-12 11:49:33 +0700

    First commit

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "127.0.0.1";
+$db_name = "clinic";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
+?>

8ad844eac5659ea0dcd76bc651c7b92879eaa284
Khanh <lamthidiemkhanh@gmail.com>
2025-09-13 11:33:10 +0700

    settings

--- nav.js
+++ nav.js
@@ -10,6 +10,7 @@
     if (items[1]) items[1].addEventListener('click', function(){ go('notifications.html'); });
     // Appointments
     if (items[2]) items[2].addEventListener('click', function(){ go('appointments.html'); });
+    // Settings
+    if (items[3]) items[3].addEventListener('click', function(){ go('settings.html'); });
   });
 })();
-
